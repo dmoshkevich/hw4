a6538755284e1cb78b7c2c70a49fc1497f706213
dmoshkevich <63773063+dmoshkevich@users.noreply.github.com>
2021-05-22 18:57:48 +0300

    Update test_advert.py

--- test_advert.py
+++ test_advert.py
@@ -3,13 +3,22 @@ import advert
 import json
 
 
-def test_advert():
+def test_title():
+    info_str = """{
+          "title": "Вельш-корги",
+          "price": 1000
+        }"""
+    actual = advert.dict_to_obj(json.loads(info_str), advert.Advert)
+    expected_title = 'Вельш-корги'
+    assert actual.title == expected_title
+
+
+def test_nesting():
     info_str = """{
      "title": "iPhone X", 
      "price": 100,
      "location": {
-      "address": "город Самара, улица Мориса Тореза, 50",
-      "metro_stations": ["Спортивная", "Гагаринская"]
+      "address": "город Самара, улица Мориса Тореза, 50"
      }   
      }"""
     actual = advert.dict_to_obj(json.loads(info_str), advert.Advert)
@@ -20,11 +29,7 @@ def test_advert():
 def test_price_below_zero():
     info_str = """{
      "title": "iPhone X", 
-     "price": -100,
-     "location": {
-      "address": "город Самара, улица Мориса Тореза, 50",
-      "metro_stations": ["Спортивная", "Гагаринская"]
-     }   
+     "price": -100 
      }"""
     with pytest.raises(ValueError):
         advert.dict_to_obj(json.loads(info_str), advert.Advert)
@@ -32,26 +37,8 @@ def test_price_below_zero():
 
 def test_no_price_in_json():
     info_str = """{
-     "title": "iPhone X", 
-     "location": {
-      "address": "город Самара, улица Мориса Тореза, 50",
-      "metro_stations": ["Спортивная", "Гагаринская"]
-     }   
+     "title": "iPhone X"
      }"""
     actual = advert.dict_to_obj(json.loads(info_str), advert.Advert)
     expected_price = 0
     assert actual.price == expected_price
-
-
-def test_title():
-    info_str = """{
-          "title": "Вельш-корги",
-          "price": 1000,
-          "class": "dogs",
-          "location": {
-            "address": "сельское поселение Ельдигинское, поселок санатория Тишково, 25"
-          }
-        }"""
-    actual = advert.dict_to_obj(json.loads(info_str), advert.Advert)
-    expected_title = 'Вельш-корги'
-    assert actual.title == expected_title
